4876cd43460610f30ccaeaa0cce2cf5875007f2c
Mario Quirós <115672555+MQuirosP@users.noreply.github.com>
2024-09-11 09:01:49 -0600

    modified:   backend/src/utils/responseHandler.ts

--- backend/src/utils/responseHandler.ts
+++ backend/src/utils/responseHandler.ts
@@ -1,34 +1,37 @@
-import { Response } from "express";
+// se cambió por un middleware que sobreescribe las
+// respuestas del servidor
+// import { Response } from "express";
 
-export class ResponseHandler {
-  static sendSuccessResponse(
-    res: Response,
-    data: any,
-    message?: string,
-    statusCode: number = 200
-  ) {
-    if (!res.headersSent) {
-      res.status(statusCode).json({
-        success: true,
-        message: message || "Request successfull",
-        data,
-        statusCode: statusCode,
-      });
-    }
-  }
 
-  static sendErrorResponse(
-    res: Response,
-    error: any,
-    statusCode: number = 500
-  ) {
-    if (!res.headersSent) {
-      res.status(statusCode).json({
-        success: false,
-        message: error.message || "Internal Server Error",
-        statusCode: statusCode,
-      });
-    }
-  }
+// export class ResponseHandler {
+//   static sendSuccessResponse(
+//     res: Response,
+//     data: any,
+//     message?: string,
+//     statusCode: number = 200
+//   ) {
+//     if (!res.headersSent) {
+//       res.status(statusCode).json({
+//         success: true,
+//         message: message || "Request successfull",
+//         data,
+//         statusCode: statusCode,
+//       });
+//     }
+//   }
 
-}
+//   static sendErrorResponse(
+//     res: Response,
+//     error: any,
+//     statusCode: number = 500
+//   ) {
+//     if (!res.headersSent) {
+//       res.status(statusCode).json({
+//         success: false,
+//         message: error.message || "Internal Server Error",
+//         statusCode: statusCode,
+//       });
+//     }
+//   }
+
+// }
