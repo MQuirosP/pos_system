94e91f5ff3ca98ae94e67fa1d27e83a2ec18a58a
Mario Quirós <115672555+MQuirosP@users.noreply.github.com>
2024-09-12 11:46:35 -0600

    modified:   backend/src/middlewares/validateDTO.ts

--- backend/src/middlewares/validateDTO.ts
+++ backend/src/middlewares/validateDTO.ts
@@ -3,19 +3,24 @@ import { Request, Response, NextFunction } from "express";
 import { AppError } from "./errorHandler";
 
 export const validateDTO = (DTOClass: any) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            // const expectedKeys = DTOClass.expectedKeys;
-            const receivedKeys = Object.keys(req.body)
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // const expectedKeys = DTOClass.expectedKeys;
+      const receivedKeys = Object.keys(req.body);
 
-            DTOClass.validateKeys(receivedKeys)
+      DTOClass.validateKeys(receivedKeys);
 
-            const dto = new DTOClass(req.body);
-            await validateOrReject(dto);
-            req.body = dto;
-            next();
-        } catch (error) {
-            next(new AppError("Validation error: certain required properties or values are missing.", 400))
-        }
+      const dto = new DTOClass(req.body);
+      await validateOrReject(dto);
+      req.body = dto;
+      next();
+    } catch (error) {
+      next(
+        new AppError(
+          "Validation error: certain required properties or values are missing.",
+          400
+        )
+      );
     }
-}
+  };
+};
