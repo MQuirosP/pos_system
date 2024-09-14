7c052f720239a9da860d517ecd4b6af2258a22bf
Mario Quirós <115672555+MQuirosP@users.noreply.github.com>
2024-09-14 12:15:45 -0600

    modified:   backend/src/middlewares/databaseErrorHandler.ts

--- backend/src/middlewares/databaseErrorHandler.ts
+++ backend/src/middlewares/databaseErrorHandler.ts
@@ -3,15 +3,26 @@ import { AppError } from "./errorHandler";
 
 export function handleDatabaseError(error: any): void {
   if (error instanceof QueryFailedError) {
-    if ((error as any).code === "23505") {
-      throw new AppError("User already exists.", 409);
-    }
-    if ((error as any).code === "23514") {
-      throw new AppError("Invalid data. Some fields cannot be empty.", 400);
-    }
-    if ((error as any).code === 'P0001') {  
-      throw new AppError('The username cannot be modified.', 500);
+    const code = (error as any).code;
+
+    switch (code) {
+      case "23505":
+        throw new AppError("User already exists.", 409);
+      case "23514":
+        throw new AppError("Invalid data. Some fields cannot be empty.", 400);
+      case "P0001":
+        throw new AppError(
+          `The username cannot be modified. Details: ${
+            (error as any).where || ""
+          }`,
+          500
+        );
+      default:
+        // Mensaje genérico para excepciones de PL/pgSQL
+        throw new AppError("A database error occurred: " + error.message, 500);
     }
   }
-  throw new AppError(error, 500);
+
+  // Manejo de errores no específicos
+  throw new AppError("An unexpected error occurred: " + error.message, 500);
 }
