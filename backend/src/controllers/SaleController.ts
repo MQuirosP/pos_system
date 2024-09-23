df7d2df67107dda2bbbff99234403be1a38ef1da
Mario Quirós <115672555+MQuirosP@users.noreply.github.com>
2024-09-23 11:02:54 -0600

    modified:   backend/src/controllers/SaleController.ts

--- backend/src/controllers/SaleController.ts
+++ backend/src/controllers/SaleController.ts
@@ -34,18 +34,18 @@ export class SaleController {
 
   async fetchAllSales(req: Request, res: Response, next: NextFunction) {
     try {
-        const sales = await this.saleService.fetchSales();
-        
-        if (sales.length === 0) {
-            return res.success(sales, "No sales found.", 200);
-        }
+      const sales = await this.saleService.fetchSales();
 
-        const saleResponseDTO = sales.map(sale => new SaleResponseDto(sale));
-        return res.success(saleResponseDTO, "Sales fetched successfully.", 200);
+      if (sales.length === 0) {
+        return res.success(sales, "No sales found.", 200);
+      }
+
+      const saleResponseDTO = sales.map((sale) => new SaleResponseDto(sale));
+      return res.success(saleResponseDTO, "Sales fetched successfully.", 200);
     } catch (error) {
-        next(error);
+      next(error);
     }
-}
+  }
 
   async fetchSaleByDocNumber(req: Request, res: Response, next: NextFunction) {
     try {
@@ -54,7 +54,7 @@ export class SaleController {
       if (!sale) {
         return res.error({ message: "Sale not found." }, 404);
       }
-      const saleResponseDto = new SaleResponseDto(sale)
+      const saleResponseDto = new SaleResponseDto(sale);
       return res.success(saleResponseDto, "Sale fetched successfully.", 200);
     } catch (error) {
       next(error);
