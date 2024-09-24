a90bafe7405228708463b0e85e5298b2fb2d6235
Mario Quirós <115672555+MQuirosP@users.noreply.github.com>
2024-09-24 12:01:38 -0600

    modified:   backend/src/routes/sales.route.ts

--- backend/src/routes/sales.route.ts
+++ backend/src/routes/sales.route.ts
@@ -1,14 +1,20 @@
-import  express  from 'express';
-import { SaleController } from '../controllers/SaleController';
-import { validateDTO } from '../middlewares/validateDTO';
-import { SaleCreateDTO } from '../dtos/sales.dto';
-
+import express from "express";
+import { SaleController } from "../controllers/SaleController";
+import { validateDTO } from "../middlewares/validateDTO";
+import { SaleCreateDTO } from "../dtos/sales.dto";
 
 const router = express.Router();
 const saleController = new SaleController();
 
-router.post("/", validateDTO(SaleCreateDTO), saleController.createSale.bind(saleController));
+router.post(
+  "/",
+  validateDTO(SaleCreateDTO),
+  saleController.createSale.bind(saleController)
+);
 router.get("/", saleController.fetchAllSales.bind(saleController));
-router.get("/:doc_number", saleController.fetchSaleByDocNumber.bind(saleController));
+router.get(
+  "/:doc_number",
+  saleController.fetchSaleByDocNumber.bind(saleController)
+);
 
 export default router;
