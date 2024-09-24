2bc10954cb4e16523f644b6701ea70c56408cd33
Mario Quirós <115672555+MQuirosP@users.noreply.github.com>
2024-09-24 12:40:01 -0600

    modified:   backend/src/middlewares/databaseErrorHandler.ts

--- backend/src/middlewares/databaseErrorHandler.ts
+++ backend/src/middlewares/databaseErrorHandler.ts
@@ -2,7 +2,6 @@ import { QueryFailedError } from "typeorm";
 import { AppError } from "./errorHandler";
 
 export function handleDatabaseError(error: any): void {
-
   if (error instanceof QueryFailedError) {
     const code = (error as any).code;
 
