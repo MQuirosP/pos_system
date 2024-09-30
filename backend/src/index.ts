fa3af089617fcfeab9e536a9ea7efdc00e645df6
Mario Quirós <115672555+MQuirosP@users.noreply.github.com>
2024-09-30 15:22:37 -0600

    Modularized Index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,38 +1,40 @@
-// src/index.ts
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
-import dataSource from "./config/ormconfig";
-import { globalErrorHandler } from "./middlewares/errorHandler";
-import routes from "./routes";
-import { responseMiddleware } from "./middlewares/responseMiddleware";
+import "./server/server"
 
-dotenv.config();
+// // src/index.ts
+// import express from "express";
+// import cors from "cors";
+// import bodyParser from "body-parser";
+// import dotenv from "dotenv";
+// import dataSource from "./config/ormconfig";
+// import { globalErrorHandler } from "./middlewares/errorHandler";
+// import routes from "./routes";
+// import { responseMiddleware } from "./middlewares/responseMiddleware";
 
-const app = express();
-const PORT = process.env.APP_PORT || 3000;
+// dotenv.config();
 
-app.use(cors());
-app.use(bodyParser.json({
-  limit: '10mb',
-  strict: true,
-  type: 'application/json'
-}));
+// const app = express();
+// const PORT = process.env.APP_PORT || 3000;
 
-dataSource
-  .initialize()
-  .then(() => {
-    console.log("Connected to the database!");
+// app.use(cors());
+// app.use(bodyParser.json({
+//   limit: '10mb',
+//   strict: true,
+//   type: 'application/json'
+// }));
 
-    app.use(responseMiddleware);
-    app.use("/api/", routes);
-    app.use(globalErrorHandler);
+// dataSource
+//   .initialize()
+//   .then(() => {
+//     console.log("Connected to the database!");
 
-    app.listen(PORT, () => {
-      console.log(`Server running at PORT: ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to the database:", error);
-  });
+//     app.use(responseMiddleware);
+//     app.use("/api/", routes);
+//     app.use(globalErrorHandler);
+
+//     app.listen(PORT, () => {
+//       console.log(`Server running at PORT: ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to the database:", error);
+//   });
---
+++ backend/src/server/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { responseMiddleware } from "../middlewares/responseMiddleware";
+import routes from "../routes";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({
+    limit: "10mb",
+    strict: true,
+    type: "application/json"
+}));
+
+app.use(responseMiddleware);
+app.use("/api/", routes);
+
+export default app;
