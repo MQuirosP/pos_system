ab3d75ae413230f505fdda2340b1b092586a1a6a
MQuirosP <mquirosp78@gmail.com>
2024-10-19 15:41:15 -0600

    modified:   backend/src/config/ormconfig.ts

--- backend/src/config/ormconfig.ts
+++ backend/src/config/ormconfig.ts
@@ -20,7 +20,7 @@ const dataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "5432", 10),
-  username: process.env.DB_USER,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [
