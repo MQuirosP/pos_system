f44347ef6656eb9b4524b08ea2491e33454b3ec6
MQuirosP <mquirosp78@gmail.com>
2024-10-28 17:46:21 -0600

    Logger message added

--- backend/src/middlewares/validatePutParams.ts
+++ backend/src/middlewares/validatePutParams.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@middlewares/errorHandler";
+import logger from "../utils/logger";
 
 export function validatePutParams(
   req: Request,
@@ -10,6 +11,14 @@ export function validatePutParams(
     const paramsRegex = /\/\w+\/\d+$/;
 
     if (!paramsRegex.test(req.originalUrl)) {
+      logger.error({
+        message: `Request rejected: missing or invalid identifier in the URL path for PUT method.`,
+        method: req.method,
+        url: req.originalUrl,
+        statusCode: res.statusCode,
+        clientIp: req.ip,
+        // userId: req.user?.id || "Anonymous"
+      });
       return next(
         new AppError(
           "A valid identifier is required in the URL for PUT requests.",
